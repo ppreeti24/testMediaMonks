95793250b0af8340c0d740f804276ac63a5ae97f
XOXODAYADMIN <xoxodayadmin@PreetiPatidars-MacBook-Air.local>
2019-03-26 21:47:55 +0530

    3 ViewControllers added
    View controllers name
    *AlbumListVC
    *PhotoListVC
    *PhotoDetailVC
    and also 3 ViewControllers added in stroyboard

---
+++ test/test/Controller/PhotoDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by XOXODAYADMIN on 26/03/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- test/test/Controller/PhotoListVC.swift
+++ test/test/Controller/PhotoListVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
